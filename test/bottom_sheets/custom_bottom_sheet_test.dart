f638bc9f37f66a3f1fcb910742b2de6abe523d92
Marcos Toranzo <marcos.toranzo11@gmail.com>
2023-05-02 22:40:44 +0200

    Add tests for CustomBottomSheet

---
+++ test/bottom_sheets/custom_bottom_sheet_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_test_stacked/app/app.bottomsheets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import '../helpers/test_helpers.dart';
+
+void main() {
+  group('CustomBottomSheet Tests- ', () {
+    setUpAll(() {
+      testWidgetSetUpAll(
+        setUpBottomSheetUi: true,
+      );
+    });
+
+    setUp(() {
+      setUpServices();
+    });
+
+    tearDown(tearDownServices);
+
+    testWidgets(
+      'should display data',
+      testWidget(
+        scaffoldBodyBuilder: (context) => TextButton(
+          onPressed: () {
+            final BottomSheetService bottomSheetService = getService();
+
+            bottomSheetService.showCustomSheet(
+              variant: BottomSheetType.custom,
+              data: const Text('text'),
+            );
+          },
+          child: const Text('button'),
+        ),
+        (helper) async {
+          await helper.tap(TextButton);
+
+          helper.text('button');
+        },
+      ),
+    );
+  });
+}
